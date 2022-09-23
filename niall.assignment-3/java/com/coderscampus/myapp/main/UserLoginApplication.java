c14611b072e263b588a21ac335f2644c1a0cac19
Niall-J-Murray <niall_murray@outlook.com>
2022-09-23 17:28:21 +0100

    Initial commit

---
+++ niall.assignment-3/java/com/coderscampus/myapp/main/UserLoginApplication.java
@@ -0,0 +1,35 @@
+package com.coderscampus.myapp.main;
+
+// *Note, my commenting on this assignment may be deemed excessive.
+// This is an unfortunate habit I have picked up from previous training.
+// I will correct this for future assignments!*
+import com.coderscampus.myapp.service.UserService;
+
+public class UserLoginApplication {
+
+	private UserService userService = new UserService();
+
+	// Minimal user login app containing main method, which only calls a in in class
+	// method to execute the program.
+	public static void main(String[] args) {
+		new UserLoginApplication().execute();
+	}
+
+	// Execute method to call required methods from
+	private void execute() {
+		// Existing user data read from "data.txt" stored as String.
+		String userDataString = userService.readUserData();
+		// Get number of users to pass to user array builder method to define array
+		// length.
+		int numberOfUsers = userService.getNumberOfUsers();
+		// Array of User objects declared by assigning return value of buildUserArray to
+		// variable.
+		User[] registeredUsers = userService.buildUserArray(userDataString, numberOfUsers);
+		// Lines 13-17 could be condensed as below, but it is less readable.
+		// *Is this best practice?*
+//		User[] registeredUsers = userService.buildUserArray(userService.readUserData(), userService.getNumberOfUsers());
+		// Pass User object array to login method to take user input and compare to
+		// known user credentials.
+		userService.userLogin(registeredUsers);
+	}
+}
